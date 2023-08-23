6b42b92901f1fe61eec7e337088453849a4be215
Rodrigo Rodrigues <rs9844660@gmail.com>
2023-08-23 14:36:17 -0300

    fix: limitações de casas decimais

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function atualizarTabela() {
 function atualizarMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resuldado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
